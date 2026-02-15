64be23bd4a9ef88c36a7137062af72836e3d77e6
Sven Göthel <sgothel@jausoft.com>
2026-02-15 05:05:43 +0100

    GLSL: Fix matSpecular constant (default)

--- resources/impl/graph/glsl/constants.glsl
+++ resources/impl/graph/glsl/constants.glsl
@@ -7,12 +7,9 @@
     // Defining The Material Colors
     const vec4 matAmbient  = vec4(0.2, 0.2, 0.2, 1.0); // orig default
     const vec4 matDiffuse  = vec4(0.8, 0.8, 0.8, 1.0); // orig default
-    // const vec4 matAmbient  = vec4(0.3, 0.3, 0.3, 1.0);
-    // const vec4 matDiffuse  = vec4(0.7, 0.7, 0.7, 1.0);
 
     // const vec4 matSpecular = vec4(0.0, 0.0, 0.0, 1.0); // orig default
-    // const vec4 matSpecular = vec4(0.8, 0.8, 0.8, 1.0);
-    const vec4 matSpecular = vec4(0.0, 0.8, 0.8, 1.0);
+    const vec4 matSpecular = vec4(0.8, 0.8, 0.8, 1.0);
 
     // const float matShininess = 0.0; // orig default
     const float matShininess = 0.5;
